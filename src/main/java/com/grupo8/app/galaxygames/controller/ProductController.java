596e44caae511a04aaf94f9965a269d917483911
AbadDev <abadcastro424@gmail.com>
2024-11-08 20:06:52 -0600

    Update product done

--- src/main/java/com/grupo8/app/galaxygames/controller/ProductController.java
+++ src/main/java/com/grupo8/app/galaxygames/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.grupo8.app.galaxygames.controller;
 
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +43,24 @@ public class ProductController {
         User user = new User(1,"","","","","","", "");
         product.setUser(user);
         productService.save(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Product product = new Product();
+        Optional<Product> optionalProduct = productService.get(id);
+        product = optionalProduct.get();
+
+        LOGGER.info("Producto buscado: {}", product);
+        model.addAttribute("product", product);
+
+        return "products/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product){
+        productService.update(product);
         return "redirect:/product";
     }
 
